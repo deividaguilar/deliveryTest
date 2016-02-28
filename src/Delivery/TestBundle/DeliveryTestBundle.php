83bbd48303406ca5cc491dba387b18cafe857b14
daguilar <daguilar@TI1VNH>
2016-02-28 04:15:09 -0500

    Ajuste para configuracion de plantillas de login y registro

--- src/Delivery/TestBundle/DeliveryTestBundle.php
+++ src/Delivery/TestBundle/DeliveryTestBundle.php
@@ -4,6 +4,10 @@ namespace Delivery\TestBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class DeliveryTestBundle extends Bundle
-{
+class DeliveryTestBundle extends Bundle {
+
+    public function getParent() {
+        return 'FOSUserBundle';
+    }
+
 }
